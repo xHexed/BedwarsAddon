feeae4941b398ba4609f887abfda4a1b58e87e6f
xHexed <minhquanpct7b@gmail.com>
2020-07-08 20:32:02 +0800

    Fix CommandHandler

--- src/com/grassminevn/bwaddon/CommandHandler.java
+++ src/com/grassminevn/bwaddon/CommandHandler.java
@@ -3,10 +3,11 @@ package com.grassminevn.bwaddon;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class CommandHandler implements CommandExecutor {
     public boolean onCommand(final CommandSender sender, final Command command, final String label, final String[] args) {
-        if (args.length < 1 || sender.hasPermission("admin")) return false;
+        if (args.length < 1 || (sender instanceof Player && !sender.hasPermission("admin"))) return false;
         switch (args[0].toLowerCase()) {
             case "reload":
                 BedwarsAddon.getInstance().reloadConfig();
